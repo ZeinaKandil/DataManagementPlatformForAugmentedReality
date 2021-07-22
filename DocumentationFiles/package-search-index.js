34f4cbe4dff6995a3cb93e9364eb6ef2cbb92abc
ZeinaKandil <zeinak99@gmail.com>
2021-07-22 21:31:37 +0200

    Apparently the documentation was not uploaded previously.

---
+++ DocumentationFiles/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mongodb.AR.dataManagementPlatform"},{"l":"com.mongodb.AR.dataManagementPlatform.csfle"}];updateSearchResults();
